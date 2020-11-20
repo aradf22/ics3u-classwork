0986f43177e3a933e3068b2a9eb015bff0d41008
aradf22 <74620026+aradf22@users.noreply.github.com>
2020-11-20 21:47:59 +0330

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -6,3 +6,10 @@ cans = int(input("How many cans come in 1 package? "))
 pack = int(input("How many packs are there? "))
 num = cans * pack
 print ("You have purchased a total of", num, "cans ")
+
+
+base = int(input("What's the length of the base of the prism? In meters: "))
+width = int(input("What's the width of the prism? In meters: "))
+height = int(input("What's the height of the prism? In meters: "))
+volume = base * width * height
+print("If those are all true, then your prism should have a volume of", volume, "meters cubed")
