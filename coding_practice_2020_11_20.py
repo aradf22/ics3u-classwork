ea2b18885e0a31250a65c6617368edd60579440a
aradf22 <74620026+aradf22@users.noreply.github.com>
2020-11-20 21:37:53 +0330

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,2 +1,8 @@
 colour = (input("What's your favorite colour? "))
 print("Really? I hate ", colour)
+
+
+cans = int(input("How many cans come in 1 package? "))
+pack = int(input("How many packs are there? "))
+num = cans * pack
+print ("You have purchased a total of", num, "cans ")
