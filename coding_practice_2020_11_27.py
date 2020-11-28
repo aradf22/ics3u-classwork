5884d09fce18564449c6cd7692c45f06a6f377d3
aradf22 <74620026+aradf22@users.noreply.github.com>
2020-11-28 22:48:06 +0330

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,73 @@
+number = 457
+while number <= 691 and number >= 457:
+  print(number)
+  number = number + 13
+
+number = -396
+while number <= -22 and number >= -396:
+  print(number)
+  number = number + 17
+  if number > -22:
+    break
+
+number = -242
+while number <= -74 and number >= -242:
+  print(number)
+  number = number + 4
+  if number > -74:
+    break
+
+number = -352 
+while number <= -64 and number >= -352:
+  print(number)
+  number = number + 16
+  if number > -64:
+    break
+
+number = 53
+while number <= 359 and number >= 53:
+  print(number)
+  number = number + 9
+  if number > 359:
+    break
+
+number = -162 
+while number <= -100 and number >= -162:
+  print(number)
+  number = number + 2
+  if number > -100:
+    break
+ 
+number = 122 
+while number <= 192 and number >= 122:
+  print(number)
+  number = number + 7
+  if number > 192:
+    break
+
+number = 66 
+while number <= 210 and number >= 66:
+  print(number)
+  number = number + 3
+  if number > 210:
+    break
+
+number = 49
+while number <= 576 and number >= 49:
+  print(number)
+  number = number + 17
+  if number > 576:
+    break
+
+number = -36  
+while number <= 477 and number >= -36:
+  print(number)
+  number = number + 19
+  if number > 477:
+    break
+
+number = -333 
+while number <= -157 and number >= -333:
+  print(number)
+  number = number + 8
+  
